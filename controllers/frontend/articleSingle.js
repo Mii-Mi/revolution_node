ad6154af5f4188fc4f91097dc8918bb9d8a520ec
Cécile <peigne.cecile@free.fr>
2019-05-22 21:00:30 +0200

    commentOwner edit ok

--- controllers/frontend/articleSingle.js
+++ controllers/frontend/articleSingle.js
@@ -17,20 +17,29 @@ module.exports = async (req, res) => {
         }
         
         await Comments.find({articleId: article._id}, (error, comment) => {
-            console.log(comment);
             
-            if (comment.authorId === req.session.userId){
-                isCommentOwner = true
+            for( i = 0; i < comment.length; i++){
+                if (comment[i].authorId === req.session.userId){
+
+                    comment[i] = {
+                        createDate: comment[i].createDate,
+                        _id: comment[i]._id,
+                        content: comment[i].content,
+                        author: comment[i].author,
+                        authorId: comment[i].authorId,
+                        articleId: comment[i].articleId,
+                        isCommentOwner: true
+                    }
+                }
             }
-
-
-            if (group == 'admin') {
+            
+            if (group === 'admin') {
                 const admin = true
-                return res.render('frontendView/articleSingle', { admin, isOwner, isCommentOwner, article, comment });
+                return res.render('frontendView/articleSingle', { admin, isOwner, article, comment });
                 
-            } else if (group == 'member') {
+            } else if (group === 'member') {
                 const member = true
-                return res.render('frontendView/articleSingle', { member, isOwner, isCommentOwner, article, comment });
+                return res.render('frontendView/articleSingle', { member, isOwner, article, comment });
                 
             } else {
                 return res.render('frontendView/articleSingle', { article, comment });
@@ -38,11 +47,4 @@ module.exports = async (req, res) => {
 
         })
     });
-
-    
-
-
-    
-    
-    
 }
