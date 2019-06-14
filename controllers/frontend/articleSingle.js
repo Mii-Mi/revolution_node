a792c241f3e55c4806b0bc92f775ba96315101a6
cecile <peigne.cecile@free.fr>
2019-06-14 15:51:56 +0200

    moderation part 2

--- controllers/frontend/articleSingle.js
+++ controllers/frontend/articleSingle.js
@@ -1,10 +1,9 @@
 const Articles = require('../../models/Articles'),
-      Comments = require('../../models/Comments')
+      Comments = require('../../models/Comments'),
+      Users = require('../../models/User')
 
 module.exports = async (req, res) => {
     
-    let isOwner = false
-
     Articles.findById(req.params.articleId, async (error, article) => {
         if(error){
             console.log(error);
@@ -12,31 +11,94 @@ module.exports = async (req, res) => {
 
         req.session[`read` + req.params.articleId] = Date.now()
 
-        
-        if (article.authorId === req.session.userId){
-            isOwner = true;
-        }
-        
-        await Comments.find({articleId: article._id}, (error, comment) => {
-            
-            for( i = 0; i < comment.length; i++){
-                if (comment[i].authorId === req.session.userId){
+        let userIsAdmin = false,
+            userIsBanned = false,
+            userIsModo = false,
+            userIsMember = false,
+            isOwner = false
 
-                    comment[i] = {
-                        createDate: comment[i].createDate,
-                        _id: comment[i]._id,
-                        content: comment[i].content,
-                        author: comment[i].author,
-                        authorId: comment[i].authorId,
-                        articleId: comment[i].articleId,
-                        formatDate: comment[i].formatDate,
-                        isCommentOwner: true
-                    }
+        Users.findById(article.authorId, async (error, usr) => {
+            // console.log(req.flash('data')[0]);
+            if (error) {
+                console.log(error);
+
+            }
+
+            if (usr) {
+                if (usr.userGroup === 0) {
+                    userIsAdmin = true
+                } else if (usr.userGroup === 1) {
+                    userIsMember = true
+                } else if (usr.userGroup === 2) {
+                    userIsModo = true
+                } else if (usr.userGroup === 3) {
+                    userIsBanned = true
                 }
+            } else {
+                userIsBanned = true
+            }
+
+            if (article.authorId === req.session.userId){
+                isOwner = true;
             }
             
-            res.render('frontendView/articleSingle', { isOwner, article, comment })
+            await Comments.find({articleId: article._id}, async (error, comment) => {
+                
+                for( i = 0; i < comment.length; i++){
 
+                    const comBody = {createDate: comment[i].createDate, _id: comment[i]._id, content: comment[i].content, author: comment[i].author, authorId: comment[i].authorId, articleId: comment[i].articleId, formatDate: comment[i].formatDate}
+
+                    if (comment[i].authorId === req.session.userId){
+
+                        comment[i] = {
+                            ...comBody,
+                            isCommentOwner: true
+                        }
+                    }else{
+                        await Users.findById(comment[i].authorId, (error, comSender) => {
+                            
+                            if(error){
+                                console.log(error);
+                            }
+
+                            switch (comSender.userGroup) {
+                                case 0:
+                                    comment[i] = {
+                                        ...comBody,
+                                        adminComment: true
+                                    }
+                                    break;
+                                case 1:
+                                    comment[i] = {
+                                        ...comBody,
+                                        memberComment: true
+                                    }
+                                    break;
+                                case 2:
+                                    comment[i] = {
+                                        ...comBody,
+                                        modoComment: true
+                                    }
+                                    break;
+                                case 3:
+                                    comment[i] = {
+                                        ...comBody,
+                                        bannedComment: true
+                                    }
+                                    break;
+                            
+                                default:
+                                    comment[i] = {
+                                        ...comBody,
+                                        bannedComment: true
+                                    }
+                                    break;
+                            }
+                        })
+                    }
+                }
+                res.render('frontendView/articleSingle', { isOwner, userIsAdmin, userIsModo, userIsMember, userIsBanned, article, comment })
+            })
         })
     });
 }
--- controllers/frontend/articlesDisplay.js
+++ controllers/frontend/articlesDisplay.js
@@ -3,46 +3,71 @@ const Users = require('../../models/User'),
 
 module.exports = async (req, res) => {
 
-    Users.find( async (error, usr) => {
-        // console.log(req.flash('data')[0]);
-        if (error) {
-            console.log(error);
+    await Articles.find({}, null, { sort: { tStamp: -1 } }, async (err, article) => {
+
+        if(err){
+            console.log(err);
         }
+        for (i = 0; i < article.length; i++) {
 
-        await Articles.find({}, null, { sort: { tStamp: -1 } }, (err, article) => {
+            let artBody = {_id: article[i]._id, title: article[i].title, content: article[i].content, author: article[i].author, authorId: article[i].authorId, createDate: article[i].createDate}
 
-            if(err){
-                console.log(err);
-            }
-            for (i = 0; i < article.length; i++) {
-                
+            if (article[i].tStamp >= req.session.lastVisit) {
+                if (req.session['read' + article[i]._id] && req.session['read' + article[i]._id] >= article[i].tStamp) {
 
-                if (article[i].tStamp >= req.session.lastVisit) {
-                    if (req.session['read' + article[i]._id] && req.session['read' + article[i]._id] >= article[i].tStamp) {
-
-                        article[i] = {
-                            _id: article[i]._id,
-                            title: article[i].title,
-                            content: article[i].content,
-                            author: article[i].author,
-                            authorId: article[i].authorId,
-                            createDate: article[i].createDate,
-                            read: true
-                        }
-                    }
-                } else {
                     article[i] = {
-                        _id: article[i]._id,
-                        title: article[i].title,
-                        content: article[i].content,
-                        author: article[i].author,
-                        authorId: article[i].authorId,
-                        createDate: article[i].createDate,
+                        ...artBody,
                         read: true
                     }
                 }
+                console.log(article[i]);
+                
+            } else {
+                article[i] = {
+                    ...artBody,
+                    read: true
+                }
+                console.log(article[i]);
+                
             }
-            res.render('frontendView/articles', { usr, article })
-        })
+            await Users.findById(article[i].authorId, (error, usr) =>{
+                
+                switch (usr.userGroup) {
+                    case 0:
+                        article[i] ={
+                            ...artBody,
+                            read: article[i].read,
+                            adminArt: true
+                        }
+                        break;
+                    case 1:
+                        article[i] ={
+                            ...artBody,
+                            read: article[i].read,
+                            memberArt: true
+                        }
+                        break;
+                    case 2:
+                        article[i] ={
+                            ...artBody,
+                            read: article[i].read,
+                            modoArt: true
+                        }
+                        break;
+                    case 3:
+                        article[i] ={
+                            ...artBody,
+                            read: article[i].read,
+                            bannedArt: true
+                        }
+                        break;
+                
+                    default:
+                        console.log('no userGroup');
+                        break;
+                }
+            })
+        }
+        res.render('frontendView/articles', { article })
     })
 }
