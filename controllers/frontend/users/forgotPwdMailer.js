9616b1311d917bed184dc0e5d4ebbc42639eee82
cecile <peigne.cecile@free.fr>
2019-07-08 16:11:01 +0200

    test

--- controllers/frontend/users/forgotPwdMailer.js
+++ controllers/frontend/users/forgotPwdMailer.js
@@ -40,7 +40,7 @@ module.exports = (req, res) => {
             
             let mailOptions = {
                 from: '"miimi" <miimi@resistance.cf>', // sender address
-                to: "test-9v4ji@mail-tester.com", // list of receivers
+                to: "24340@SpamScoreChecker.com", // list of receivers
                 subject: "Mot de passe oublié", // Subject line
                 text: `Pour réinitialiser votre mot de passe, veuillez cliquer le lien suivant, ou le copier dans la barre d'url de votre navigateur.\n
                        resistance.cf/users/password/edit/${lnk}`, // plain text body
