35a714b5eb867b59a10ea152bebe490149d289a8
cecile <peigne.cecile@free.fr>
2019-07-09 14:35:53 +0200

    test

--- controllers/frontend/users/userPwdUpdate.js
+++ controllers/frontend/users/userPwdUpdate.js
@@ -5,15 +5,31 @@ module.exports = (req, res) => {
     const usr = Users.findById(req.params.usrId)
 
     if (req.params.usrTstamp === usr.lastVisit && req.body.pass === req.body.pass2){
-        Users.findByIdAndUpdate(usr._id, { 'pass': bcrypt.hash(req.body.pass, 10, (err, encrypted) => {`pass: ${encrypted}`})}, (err, upUser) => {
-            if (err) {
-                console.log(err);
-                req.flash('error', 'Une erreur est survenue... veuillez réessayer.')
-                res.redirect('/')
-            }else{
-                req.flash('succes', 'Succès ! Vous pouvez maintenant vous connecter avec votre nouveau mot de passe !')
-                res.redirect('/')
-            }
+
+        password = req.body.pass
+        bcrypt.hash(password, 10, (hash) => {
+            req.body.password = hash
+
+            Users.findByIdAndUpdate(usr._id, { 'pass': req.body.pass}, (err, upUser) => {
+                if (err) {
+                    console.log(err);
+                    req.flash('error', 'Une erreur est survenue... veuillez réessayer.')
+                    res.redirect('/')
+                }else{
+                    req.flash('succes', 'Succès ! Vous pouvez maintenant vous connecter avec votre nouveau mot de passe !')
+                    res.redirect('/')
+                }
+            })
         })
     }
 }
+
+
+
+    UserSchema.pre('save', function (next) {
+        const user = this;
+        bcrypt.hash(user.pass, 10, (error, encrypted) => {
+            user.pass = encrypted;
+            next()
+        })
+    })
