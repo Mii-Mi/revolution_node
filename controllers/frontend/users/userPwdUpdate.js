3a11b9714ebe7e84a5be8059734437043b826202
cecile <peigne.cecile@free.fr>
2019-07-09 12:03:11 +0200

    test

--- controllers/frontend/users/userPwdUpdate.js
+++ controllers/frontend/users/userPwdUpdate.js
@@ -4,7 +4,7 @@ module.exports = (req, res) => {
     const usr = Users.findById(req.params.usrId)
 
     if (req.params.usrTstamp === usr.lastVisit && req.body.pass === req.body.pass2){
-        usr.update({'pass': req.body.pass}, (err, pass) => {
+        Users.findByIdAndUpdate(usr._id, {'pass': req.body.pass}, (err, pass) => {
             if (err) {
                 console.log(err);
                 req.flash('error', 'Une erreur est survenue... veuillez réessayer.')
