4bb767d6cf8bc92b3f86f11ab11ad1989eff3756
cecile <peigne.cecile@free.fr>
2019-07-11 14:03:32 +0200

    just forgotten {}

--- controllers/frontend/users/forgotPwdForm.js
+++ controllers/frontend/users/forgotPwdForm.js
@@ -1,4 +1,4 @@
 module.exports = (req, res) => {
-    let title = 'Mot de pass oublié : saisie adresse mail - '
-    res.render('frontendView/users/forgotPwdForm', title)
+    let title = 'Mot de passe oublié - saisie adresse mail - '
+    res.render('frontendView/users/forgotPwdForm', {title})
 }
