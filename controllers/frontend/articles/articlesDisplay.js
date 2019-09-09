25a4b7e025d32c187ec94dbdbb3f09fa1506d155
Cécile <peigne.cecile@free.fr>
2019-09-09 11:06:45 +0200

    change async to articleDisplay ctrlr

--- controllers/frontend/articles/articlesDisplay.js
+++ controllers/frontend/articles/articlesDisplay.js
@@ -5,7 +5,7 @@ module.exports = async (req, res) => {
 
     let title = 'Fil d\'actualités - '
 
-    await Articles.find({}, null, { sort: { tStamp: -1 } }, async (err, article) => {
+    Articles.find({}, null, { sort: { tStamp: -1 } }, async (err, article) => {
 
         if(err){
             console.log(err);
